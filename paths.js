ff671331c40a7c3e257e09f4747735b086796458
Bricks <76704453+Bricks666@users.noreply.github.com>
2022-10-03 04:16:02 +0300

    test

--- paths.js
+++ paths.js
@@ -3,7 +3,7 @@ const path = require('path');
 const root = __dirname;
 const src = path.resolve(root, 'src');
 const public = path.resolve(root, 'public');
-const build = path.resolve(root, 'build');
+const build = path.resolve(public, 'build');
 
 module.exports = {
 	root,
